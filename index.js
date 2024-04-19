ad0d960a8e33ea1445b9c492380a72f75385443e
Ahmed khan <ahmed678054@gmail.com>
2024-04-19 09:47:09 -0700

    push my word counter to github

---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log('\nwelcome to my word counter:-\n');
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the words:",
+    },
+]);
+const words = answer.sentence.trim().split(" ");
+console.log(words);
+console.log(`your sentence words count is: ${words.length}`);
+console.log("\nThanks for using my word counter");
